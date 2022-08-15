747319cb52fd33ea26b9f5c2fc5508c6cf860270
abhishekagrahari123 <69042089+abhishekagrahari123@users.noreply.github.com>
2022-08-15 12:56:21 +0530

    Updated settings

--- printing_portal/settings.py
+++ printing_portal/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)2$)6b24#0(tg(1#0ljaf7a%s_cr9i8$j@l+v#d9-li#1b48hb'
+# SECRET_KEY = 'django-insecure-)2$)6b24#0(tg(1#0ljaf7a%s_cr9i8$j@l+v#d9-li#1b48hb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 CRISPY_FAIL_SILENTLY = not DEBUG 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['printing-portal.herokuapp.com']
 
 
 # Application definition
