06dfc09dfa0b09063b5d99215b1253a3d3e23091
abhishekagrahari123 <69042089+abhishekagrahari123@users.noreply.github.com>
2022-08-15 19:39:58 +0530

    assigned media root

--- printing_portal/settings.py
+++ printing_portal/settings.py
@@ -124,14 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = 
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/staticfiles/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
