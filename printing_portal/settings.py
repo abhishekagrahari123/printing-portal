0b2e03249329e1d422231f429de0daaef91f16f6
abhishekagrahari123 <69042089+abhishekagrahari123@users.noreply.github.com>
2022-08-16 10:03:40 +0530

    changed region name and some configurations

--- printing_portal/settings.py
+++ printing_portal/settings.py
@@ -139,9 +139,10 @@ AWS_URL=f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_S3_ADDRESSING_STYLE = 'virtual'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = False
 
 STATIC_URL = AWS_URL + '/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
